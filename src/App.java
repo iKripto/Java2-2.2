fc3f90e6d4417f8bc1cdcb56a90120f38a0acff0
iKripto <yagosiqueira450@gmail.com>
2025-06-08 21:32:26 -0300

    Criacao do app.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Base do retangulo: ");
+        double base = sc.nextDouble();
+
+        System.out.print("Altura do retangulo: ");
+        double altura = sc.nextDouble();
+
+        Retangulo r = new Retangulo(base, altura);
+
+        System.out.printf("AREA = %.4f\n", r.calcularArea());
+        System.out.printf("PERIMETRO = %.4f\n", r.calcularPerimetro());
+        System.out.printf("DIAGONAL = %.4f\n", r.calcularDiagonal());
+
+        sc.close();
     }
 }
